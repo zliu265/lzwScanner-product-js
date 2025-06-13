502e7fca61be6af69882152ba6f8b8edd921572e
zhongwei265 <58840653+zliu265@users.noreply.github.com>
2025-06-13 09:39:42 +0800

    尝试获取JSON的第一个close数据

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 //该文件用于打印最后结果用的
+// main.js
+const fs = require('fs');
+
+// 读取并解析 JSON 文件
+const json = JSON.parse(fs.readFileSync('AAPL.json', 'utf-8'));
+
+// 访问第一个记录的 close 值
+const firstClose = json.data[0].close;
+
+console.log('First close:', firstClose);
