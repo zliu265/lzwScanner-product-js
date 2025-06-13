11846a6ffbeb84bcc1ffe088f8bf09e5fd477bc2
Zhongwei Liu <58840653+zliu265@users.noreply.github.com>
2025-06-13 11:12:48 +0800

    Add breakout detection and ATR indicator

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const config = require('./config');
 
 const stageOneFilter = require('./src/stages/stageOneFilter');
 const stageTwoVCP = require('./src/stages/stageTwoVCP');
+const stageThreeBreakout = require('./src/stages/stageThreeBreakout');
 
 const json = JSON.parse(fs.readFileSync('AAPL.json', 'utf-8'));
 
@@ -11,4 +12,9 @@ console.log('Stage One Passed:', passed);
 if (passed) {
   const vcp = stageTwoVCP(json.data);
   console.log('Stage Two Passed:', vcp);
+  if (vcp) {
+    const todayData = json.data[json.data.length - 1];
+    const breakout = stageThreeBreakout('AAPL', todayData, json.data);
+    console.log('Stage Three Result:', breakout);
+  }
 }
